7d05ff0164952d8475decb8e180c5f88cbf50f72
unknown <komogortev@users.noreply.github.com>
2022-08-30 21:12:43 -0400

    input file mock failed

--- src/components/special-upload.vue
+++ src/components/special-upload.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 
 const imgSrc = ref(props.customImg)
 
-function fileProcess(file:File) {
+function fileProcess(file: File) {
   imgSrc.value = URL.createObjectURL(file)
 }
 
@@ -22,10 +22,10 @@ function fileUpload(event: Event): void {
   <div class="image-uploader flex bg-orange flex-col">
     <h1 class="uppercase underline p-2">File uploaded</h1>
 
-    <input @change="fileUpload" type="file" name="fileUpload" id="file-upload" accept="image/*">
+    <input @change="fileUpload" type="file" title="fileUpload" id="file-upload" accept="image/*">
 
     <div class="your-file m-2 border-2 border-solid border-orange-800 p-2">
-      <img :src="imgSrc" alt="">
+      <img id="image-preview" :src="imgSrc" alt="image-preview">
     </div>
   </div>
 </template>
