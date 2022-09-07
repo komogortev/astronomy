edc99d64214e7b1cd03f9f751002ef4fe8f00c5d
Igor Komogortev <526473+gb6@users.noreply.github.com>
2022-09-07 17:03:29 -0400

    test: public assets path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/astronomy/',
+  base: './',
   test: {
     reporters: ["json", "verbose"],
     globals: true,
